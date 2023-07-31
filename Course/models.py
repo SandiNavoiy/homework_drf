fb133dbbcfbb60893787882bba4004c446d08ce3
Sandi_Navoiy <santes.au@gmail.com>
2023-07-31 15:10:31 +0300

    2 задание первой домашки

--- Course/models.py
+++ Course/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
+NULLABLE = {'null': True, 'blank': True}
+
+
+class Course(models.Model):
+    course_name = models.CharField(max_length=100, verbose_name='название курса')
+    course_description = models.TextField(verbose_name='описание курса', **NULLABLE)
+    course_preview = models.ImageField(upload_to='course/', verbose_name='изображение', **NULLABLE)
+
+    def __str__(self):
+        return f'{self.course_name} {self.course_description}'
+
+    class Meta:
+        verbose_name = 'курс'
+        verbose_name_plural = 'курсы'
+
+
+from django.db import models
+
 # Create your models here.
--- Lesson/models.py
+++ Lesson/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+NULLABLE = {'null': True, 'blank': True}
+
+
+class Lesson(models.Model):
+    lesson_name = models.CharField(max_length=100, verbose_name='название урока')
+    lesson_description = models.TextField(verbose_name='описание урока', **NULLABLE)
+    lesson_preview = models.ImageField(upload_to='course/', verbose_name='изображение', **NULLABLE)
+    lesson_video_url = models.CharField(max_length=255, verbose_name='ссылка на видео', **NULLABLE)
+
+    def __str__(self):
+        return f'{self.lesson_name} {self.lesson_description}'
+
+    class Meta:
+        verbose_name = 'урок'
+        verbose_name_plural = 'уроки'
