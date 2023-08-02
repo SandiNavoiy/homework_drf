649940c4bf8f888604d02ed9cc7420bf3b36f68d
Sandi_Navoiy <santes.au@gmail.com>
2023-08-02 23:07:34 +0300

    1 задание

--- Course/serializers.py
+++ Course/serializers.py
@@ -4,6 +4,7 @@ from Course.models import Course
 
 
 class CourseSerializer(serializers.ModelSerializer):
+    lessons_count = serializers.IntegerField(source='Lesson_set.all.count', read_only=True)
     class Meta:
         model = Course
         fields = "__all__"
