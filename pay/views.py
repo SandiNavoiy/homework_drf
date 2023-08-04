de90fd8d81a18172f09d19489311d3a2a6412bce
Sandi_Navoiy <santes.au@gmail.com>
2023-08-04 22:53:47 +0300

    сортировка

--- pay/views.py
+++ pay/views.py
@@ -1,26 +1,33 @@
 from rest_framework import generics
+from rest_framework.filters import SearchFilter, OrderingFilter
 
-from Lesson.serializers import LessonSerializer
 from pay.models import Pay
+from pay.serializers import PaySerializer
 
 
 class PayCreateAPIView(generics.CreateAPIView):
     """Создание"""
-    serializer_class = LessonSerializer
+    serializer_class = PaySerializer
 
 class PayListAPIView(generics.ListAPIView):
     """просмотр списка"""
-    serializer_class = LessonSerializer
+    serializer_class = PaySerializer
     queryset = Pay.objects.all()
+    filter_backends = [SearchFilter, OrderingFilter]
+    #Фильтры
+    search_fields = ['payment_type', 'course_name', 'lesson_name']
+    # Сортировка прямая / обратная
+    ordering_fields = ['pay_date']
+    ordering = ['-pay_date']
 
 class PayRetrieveAPIView(generics.RetrieveAPIView):
     """Отображение элесента"""
-    serializer_class = LessonSerializer
+    serializer_class = PaySerializer
     queryset = Pay.objects.all()
 
 class PayUpdateAPIView(generics.UpdateAPIView):
     """Изменение элемента"""
-    serializer_class = LessonSerializer
+    serializer_class = PaySerializer
     queryset = Pay.objects.all()
 
 class PayDestroyAPIView(generics.DestroyAPIView):
