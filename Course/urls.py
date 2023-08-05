5c0a44195d1222ad3e0bda9d19d6c8171eac3976
Sandi_Navoiy <santes.au@gmail.com>
2023-08-05 23:47:27 +0300

    1

--- Course/urls.py
+++ Course/urls.py
@@ -1,12 +1,11 @@
 from rest_framework.routers import DefaultRouter
-
 from Course.apps import CourseConfig
 from Course.views import CourseViewSet
 
 app_name = CourseConfig.name
 
 router = DefaultRouter()
-router.register(r'Course', CourseViewSet, basename='Course')
+router.register(r'course', CourseViewSet, basename='course')
 
 urlpatterns = [
 
