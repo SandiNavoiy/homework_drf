0b6e03450aaa31747da6f17f349cafe28d338476
Sandi_Navoiy <santes.au@gmail.com>
2023-08-06 00:07:10 +0300

    нашел ошибку сериализаторе. подсчет кол-ва уроков

--- Course/serializers.py
+++ Course/serializers.py
@@ -4,7 +4,7 @@ from Course.models import Course
 
 class CourseSerializer(serializers.ModelSerializer):
     #добавление поля количество уроков  через описание нового типа, и сслыкой на запрос к лесссон, суммирование
-    lessons_count = serializers.IntegerField(source='Lesson_set.all.count', read_only=True)
+    lessons_count = serializers.IntegerField(source='lesson_set.all.count', read_only=True)
     #Добавднеие в сериализатор уроков для курса, смотри функцию get_lessons
 
     lessons = serializers.SerializerMethodField()
