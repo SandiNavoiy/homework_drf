e05a482b9ac87d4ad197556cb4e8a1e7517163e0
Sandi_Navoiy <santes.au@gmail.com>
2023-08-07 16:52:52 +0300

    25/1 исправление ошибки

--- Course/serializers.py
+++ Course/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-
+ 
 from Course.models import Course
 
 
--- config/settings.py
+++ config/settings.py
@@ -45,10 +45,18 @@ INSTALLED_APPS = [
     "Lesson",
     "pay",
     "django_filters",
-]
 
+]
 REST_FRAMEWORK = {
-    "DEFAULT_FILTER_BACKENDS": ("django_filters.rest_framework.DjangoFilterBackend")
+    'DEFAULT_FILTER_BACKENDS': (
+        'django_filters.rest_framework.DjangoFilterBackend',
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ]
 }
 
 MIDDLEWARE = [
