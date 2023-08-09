de040045b71122c520c5a6834137dbe326c90067
Sandi_Navoiy <santes.au@gmail.com>
2023-08-09 17:13:02 +0300

    Модель подписки  25.2 фикс (2)

--- Subscription/models.py
+++ Subscription/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 
 from Course.models import NULLABLE, Course
+from config import settings
 
 
 class Subscription(models.Model):
     course = models.ForeignKey(Course, on_delete=models.CASCADE, **NULLABLE, related_name='subscription', verbose_name='Подписка на курс')
-    subscriber = models.ForeignKey('users.User', on_delete=models.CASCADE, verbose_name='Подписчик', **NULLABLE)
+    subscriber = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name='Подписчик', **NULLABLE)
 
     def __str__(self):
         return f'{self.course}:{self.subscriber}'
